4f6704421755d897b90daafd2c5cec924a5372ec
Calvin James, Jr <cjames.jr@outlook.com>
2024-07-11 14:29:08 -0400

    Add files via upload

--- Java_Strings_Intro.java
+++ Java_Strings_Intro.java
@@ -8,18 +8,24 @@ public class Java_Strings_Intro {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
+        // 1. must read the two test strings
         String A = scan.nextLine();
         String B = scan.nextLine();
         
+        // 2. must sum the length of each string, and print it out
         int sum = (A.length() + B.length());
+        
         System.out.println(sum);
         
-            if (A.charAt(0) < B.charAt(0)) {
+        // 3. Must determine if A comes before B lexicographically, and print out yes or no.
+            if (A.charAt(0) <= B.charAt(0)) {
                 System.out.println("No");
             } else {
                 System.out.println("Yes");
             }
-            
+        // 4. Must capitalize the first letter of each string, separate them with a space, and print out in one line.
             System.out.println((A.substring(0, 1).toUpperCase() + A.substring(1).toLowerCase()) + " " + B.substring(0, 1).toUpperCase() + B.substring(1).toLowerCase());
-    }
+            
+            scan.close();
+    } 
 }
